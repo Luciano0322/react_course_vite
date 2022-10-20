a07ed2f8b2c857ebecdb1fc3bb1e40339a2378fe
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-10-20 11:10:08 +0800

    add note

--- beginner/src/components/Counter.tsx
+++ beginner/src/components/Counter.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from 'react'
 
 // 這裡示範簡單的 component 拆分與 props 的定義方法
 const Counter: FC<{user: string}> = ({user}) => {
+  // {user} 也可以單改用 props 去接，但在引用的同時會多一層
   // 這個是最基本的 state hook
   const [count, setCount] = useState<number>(0);
   // 有以下兩種方式來做 state change，細微上的不同會再進階的課程中提及

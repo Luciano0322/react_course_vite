acb7630e511d301dabc822a9a7c442810aecee9e
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-10-20 11:07:54 +0800

    lesson 3 示範基本的 component 拆分

--- beginner/src/App.tsx
+++ beginner/src/App.tsx
@@ -1,52 +1,16 @@
 import { useState } from "react"
+import Counter from "./components/Counter";
 
 const username = 'Luciano';
 
 function App() {
-  // 這個是最基本的 state hook
-  const [count, setCount] = useState<number>(0);
-  // 有以下兩種方式來做 state change，細微上的不同會再進階的課程中提及
-  const minus = () => setCount(count - 1);
-  const plus = () => setCount((pre) => pre + 1);
-  let num = 0;
+
   return (
     <div>
-      Hello {username}!
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: "space-around"
-        }}
-      >
-        <button onClick={minus}>-</button>
-        <p>{count}</p>
-        <button onClick={plus}>+</button>
-      </div>
-      <h5>假設不用</h5>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: "space-around"
-        }}
-      >
-        <button 
-          onClick={() => {
-            num = num - 1
-            // console.log(num);
-            // 這裡的做法是沒有辦法完成更新的，呼應前面所講的，react 本身就是採一個 V-dom
-            // 所以你的指向必須指到 component 本身，那麼以 functional component 的型態
-            // 來說就必須使用 useState hook 達成。
-            // 相同的概念回到 class component 的寫法就會很清楚了，但這裡我不多著墨。
-            return num;
-          }}
-        >
-            -
-        </button>
-        <p>{num}</p>
-        <button onClick={() => num + 1}>+</button>
-      </div>
+      <h2>
+        Hello {username}!
+      </h2>
+      <Counter user={username} />
     </div>
   )
 }
---
+++ beginner/src/components/Counter.tsx
@@ -0,0 +1,24 @@
+import { FC, useState } from 'react'
+
+// 這裡示範簡單的 component 拆分與 props 的定義方法
+const Counter: FC<{user: string}> = ({user}) => {
+  // 這個是最基本的 state hook
+  const [count, setCount] = useState<number>(0);
+  // 有以下兩種方式來做 state change，細微上的不同會再進階的課程中提及
+  const minus = () => setCount(count - 1);
+  const plus = () => setCount((pre) => pre + 1);
+  return (
+    <>
+      <h5>{`${user} play counter`}</h5>
+      <div
+        className='f-b-c'
+      >
+        <button onClick={minus}>-</button>
+        <p>{count}</p>
+        <button onClick={plus}>+</button>
+      </div>
+    </>
+  )
+}
+
+export default Counter

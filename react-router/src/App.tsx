ea4a6aecb15b5e1cdb76ac51fde90a604bc5ddfb
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-12-06 17:24:42 +0800

    uesRoutes 的使用方式與檔案拆分示範

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -8,6 +8,7 @@ import NotFonud from "./pages/NotFonud"
 import Shirts from "./pages/Shirts"
 import ShirtWid from "./pages/Shirts/ShirtWid"
 import Shop from "./pages/Shop"
+import RouteConf from "./RouteConf"
 
 // BrowserRouter 就像 redux 的 provider 一樣，必須在最上層來處理
 // Routes 裡面包的事處理所有路由導向的頁面
@@ -16,25 +17,32 @@ import Shop from "./pages/Shop"
 
 // 為了方便示範我先生成幾個頁面在 pages 的資料夾內
 const App: FC = () => {
-
+  
   return (
     <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="shop" element={<Shop />} >
-          <Route path="shirts">
-            {/* nesting 的做法 */}
-            <Route index element={<Shirts/>} />
-            <Route path=":id" element={<ShirtWid/>} />
-          </Route>
-        </Route>
-        <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<NotFonud />} />
-      </Routes>
+      <RouteConf/>
     </BrowserRouter>
   )
+  // return (
+  //   <BrowserRouter>
+  //     <Navbar/>
+  //     <Routes>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path="about" element={<About />} />
+  //       <Route path="shop" element={<Shop />} >
+  //         <Route path="shirts">
+  //           {/* nesting 的做法 */}
+  //           <Route index element={<Shirts/>} />
+  //           <Route path=":id" element={<ShirtWid/>} />
+  //         </Route>
+  //       </Route>
+  //       <Route path="contact" element={<Contact />} />
+  //       {/* 對未設定的路由可以採以下方式 */}
+  //       <Route path="*" element={<NotFonud />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // )
+  
 }
 
 export default App
---
+++ react-router/src/RouteConf.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import Layout from './components/Layout'
+import About from './pages/About'
+import Home from './pages/Home'
+import Shirts from './pages/Shirts'
+import ShirtWid from './pages/Shirts/ShirtWid'
+import Shop from './pages/Shop'
+
+// 這就是比較推薦的做法，將所有的routes拆出來做統一的管理
+// 包含了每一層的從屬關係，都可以透過下列的表很清處的看到
+// 這裡我們可以嘗試使用react-router-dom 的 <Outlet>，來處理我們常駐在畫面的navbar
+// 我這裡將這些東西拆出來用Layout的component去做封裝，類似像mantine的app shell在處理
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About />},
+      { 
+        path: "shop", 
+        element: <Shop />,
+        children: [
+          { 
+            path: "shirts", 
+            element: <Shirts/>,
+          },
+          {
+            path: "shirts/:id",
+            element: <ShirtWid/>,
+          }
+        ]
+      },
+    ]
+  },
+]
+
+const RouteConf = () => {
+  const routeConfig = useRoutes(routes)
+  return routeConfig
+}
+
+export default RouteConf

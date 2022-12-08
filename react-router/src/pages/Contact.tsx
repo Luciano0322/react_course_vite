88e21ded6205bbfed827c41f234cbdfc535f7c43
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-12-08 17:44:42 +0800

    useSearchParams 教學

--- react-router/src/pages/Contact.tsx
+++ react-router/src/pages/Contact.tsx
@@ -1,9 +1,49 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
+const members: string[] = [
+  'John',
+  'Bill',
+  'Jack',
+  'Abby',
+  'Cindy',
+];
+
+// 這裡簡單介紹一下一般常會看到的搜尋功能，就是在url後面接著參數的做法
+// 通常作為索資用，那簡單示範一下搜尋功能的做法，但實際使用上來說會串接api
 const Contact: FC = () => {
+  const [picked, setPicked] = useState<string[]>(members)
+  const [formParams, setFormParams] = useSearchParams({
+    q: '',
+  });
+
+  const q = formParams.get("q")
+
+  useEffect(() => {
+    if(q) {
+      // 如果是用來串接api的話就要在這裡置換以下的code, 並將你的 q 帶入做為參數
+      const adjMembers = members.filter((e) => e.includes(q))
+      setPicked(adjMembers)
+    }
+    return () => setPicked(members)
+  }, [q])
+
   return (
     <div>
       <h1>Contact Page</h1>
+      <form action="">
+        <fieldset>
+          <legend>choice a member</legend>
+          <input 
+            type="text" 
+            value={q || ""}  
+            onChange={(e) => setFormParams({ q: e.target.value})}
+          />
+          {picked.map((e) => (
+            <p key={e}>{e}</p>
+          ))}
+        </fieldset>
+      </form>
     </div>
   )
 }
--- react-router/src/pages/Shirts/ShirtWid.tsx
+++ react-router/src/pages/Shirts/ShirtWid.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react'
 import { useParams } from 'react-router-dom'
 
 // 可以透過useParams 的 hook 來拿到產品的 id
+// 這裡的id是對應你url定義的pathname
+// 假設你是定義成 /:shirtId 那你就要改用 shirtId 來取值
 const ShirtWid: FC = () => {
   const { id } = useParams();
   return (

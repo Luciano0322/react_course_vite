cb79d1f4d77f3f00ac940356dff6fe2fb95efcd3
Luciano Lee <luciano.lee@webcomm.com.tw>
2023-06-21 11:57:05 +0800

    react hook form lesson-init proj

---
+++ react-hook-form/README.md
@@ -0,0 +1,8 @@
+## 這篇的內容主要介紹常用表單工具React hook form  
+
+### 簡單來說就是 react 版本的form validation  
+
+---
+
+
+
---
+++ react-hook-form/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="bold text-blue-500 text-lg">表單驗證工具</h1>
+    </div>
+  )
+}
+
+export default App
